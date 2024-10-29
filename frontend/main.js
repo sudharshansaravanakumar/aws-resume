4ad9c63e558fb26af1b74bbcb01d3013bdecb397
Sudharshan Saravanakumar <sudharshan.rangas@gmail.com>
2024-10-29 14:37:36 +0530

    updated

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+
+const fuctionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(fuctionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").n=innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
